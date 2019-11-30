ba7aa974de056ea94f268ea6f3b41776a423dd11
OisinFoley <oisinfoley@yahoo.co.uk>
2019-11-30 14:19:17 -0800

    Fixing directory ommission:committing app repo which was omitted during previous commit

---
+++ course-tutorials/course-app/prj-start/src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { RecipesComponent } from "./recipes/recipes.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
+import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: 'recipes', component: RecipesComponent, children: [
+    { path: '', component: RecipeStartComponent },
+    { path: 'new', component: RecipeEditComponent },
+    // it's important to put the '/new' route before ':id',
+    // because how is angular meant to know whether you are saying
+    // interpret this as 'new', or some dynamic ':id'
+    { path: ':id', component: RecipeDetailComponent },
+    { path: ':id/edit', component: RecipeEditComponent }
+  ]},
+  { path: 'shopping-list', component: ShoppingListComponent }
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+  
+}
