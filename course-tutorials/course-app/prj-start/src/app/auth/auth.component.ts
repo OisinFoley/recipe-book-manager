70660ac6c5b8d74107eb021af9ceec3e4f82a777
OisinFoley <oisinfoley@yahoo.co.uk>
2020-04-10 23:20:59 -0700

    Implement child modules and child routing modules so that the app.modulefile is leaner (pre-lazy loading re-ordering of our declarations)

--- course-tutorials/course-app/prj-start/src/app/auth/auth.component.ts
+++ course-tutorials/course-app/prj-start/src/app/auth/auth.component.ts
@@ -1,19 +1,27 @@
-import { Component } from '@angular/core';
-import { isNullOrUndefined } from 'util';
+import { Component, ComponentFactoryResolver, ViewChild, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService, AuthResponseData } from './auth.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+
+import { AuthService, AuthResponseData } from './auth.service';
+import { AlertComponent } from '../shared/alert/alert.component';
+import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
-export class AuthComponent {
-  constructor(private auth: AuthService, private router: Router) {}
+export class AuthComponent implements OnDestroy {
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private componentFactoryResolver: ComponentFactoryResolver
+  ) {}
   isLoginMode = true;
   isLoading = false;
   error: string = null;
+  private closeSub: Subscription;
+  @ViewChild(PlaceholderDirective, { static: false }) alertHost: PlaceholderDirective;
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -43,11 +51,39 @@ export class AuthComponent {
       errorResponse => {
         console.log(errorResponse);
         this.error = errorResponse;
+        this.showErrorAlert(errorResponse);
         this.isLoading = false;
       }
     );
 
     form.reset();
+  }
+
+  onHandleError() {
+    this.error = null;
+  }
+
+  private showErrorAlert(errorResponse: string) {
+    const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(
+      AlertComponent
+    );
+
+    const hostViewContainer = this.alertHost.viewContainerRef;
+    // destroys anything that may have been embedded earlier in the dynamic component container
+    hostViewContainer.clear();
+
+    const componentRef = hostViewContainer.createComponent(alertCmpFactory);
+    componentRef.instance.message = errorResponse;
+    this.closeSub = componentRef.instance.close.subscribe(() => {
+      this.closeSub.unsubscribe();
+      hostViewContainer.clear();
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.closeSub) {
+      this.closeSub.unsubscribe();
+    }
   }
 
   // onRegister() {
