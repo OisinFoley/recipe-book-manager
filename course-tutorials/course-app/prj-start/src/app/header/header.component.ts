a96b3fefcf5aa8bb52dc73bf82a767faa7f4ca23
OisinFoley <oisinfoley@yahoo.co.uk>
2020-04-20 19:02:18 -0700

    Finish migrating all CRUD functionality for all entities to an NgRx store - remove redundant data-storage and recipe service in the process

--- course-tutorials/course-app/prj-start/src/app/header/header.component.ts
+++ course-tutorials/course-app/prj-start/src/app/header/header.component.ts
@@ -1,58 +1,48 @@
-import { Component, Output, EventEmitter, OnInit, OnDestroy } from '@angular/core';
-import { DataStorageService } from '../shared/data-storage.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { map } from 'rxjs/operators';
 
-import { AuthService } from '../auth/auth.service';
+import * as AuthActions from '../auth/store/auth.actions';
 import * as fromApp from '../store/app.reducer';
+import * as RecipeActions from '../recipes/store/recipe.actions';
 
 @Component({
-	selector: 'app-header',
-	templateUrl: './header.component.html'
+  selector: 'app-header',
+  templateUrl: './header.component.html'
 })
+
 export class HeaderComponent implements OnInit, OnDestroy {
-	isAuthenticated = false;
-	private userSub: Subscription;
-	collapsed = false;
+  private userSub: Subscription;
+  isAuthenticated = false;
+  collapsed = false;
 
-	constructor(
-		private dataStorage: DataStorageService,
-		private authService: AuthService,
-		private store: Store<fromApp.AppState>
-	) {}
-	// @Output() featureSelected = new EventEmitter<string>();
+  constructor(
+    private store: Store<fromApp.AppState>
+  ) {}
 
-	// onSelect(feature: string) {
-	// 	this.featureSelected.emit(feature);
-	// }
-	// thr above is redundant after adding routing to our app
+  ngOnInit() {
+    this.userSub = this.store
+      .select('auth')
+        .pipe(map(authState => authState.user))
+        .subscribe(user => {
+        this.isAuthenticated = !!user;
+      });
+  }
 
-	ngOnInit() {
-		this.userSub = this.store
-			.select('auth')
-			.pipe(map(authState => authState.user))
-			.subscribe(user => {
-		// coerce the opposite of !user so that we get a bool instead of the returned 'user' argument object
-			this.isAuthenticated = !!user;
-		});
-	}
+  onSaveData() {
+    this.store.dispatch(new RecipeActions.StoreRecipes());
+  }
 
+  onFetchData() {
+    this.store.dispatch(new RecipeActions.FetchRecipes());
+  }
 
-	onSaveData() {
-		this.dataStorage.storeRecipes();
-	}
-	
+  onLogout() {
+    this.store.dispatch(new AuthActions.Logout());
+  }
 
-	onFetchData() {
-		this.dataStorage.fetchRecipes().subscribe();
-	}
-
-	onLogout() {
-		this.authService.logout();
-	}
-
-	ngOnDestroy() {
-		this.userSub.unsubscribe();
-	}
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
+  }
 }
