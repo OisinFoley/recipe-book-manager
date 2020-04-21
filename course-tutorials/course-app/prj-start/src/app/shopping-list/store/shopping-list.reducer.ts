ce2c2ae3bed42abf5c09158632d52c069d7cc2ee
OisinFoley <oisinfoley@yahoo.co.uk>
2020-04-20 21:35:17 -0700

    Add Server-Side rendering support for our root route through Angular Universal

--- course-tutorials/course-app/prj-start/src/app/shopping-list/store/shopping-list.reducer.ts
+++ course-tutorials/course-app/prj-start/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -16,56 +16,56 @@ const initialState: State = {
   editedIngredientIndex: -1
 };
 
-export const ShoppingListReducer = (
-    state = initialState,
-    action: ShoppingListActions.ShoppingListActionsType
-  ) => {
-    switch (action.type) {
-      case ShoppingListActions.ADD_INGREDIENT:
-        return {
-          ...state,
-          ingredients: [...state.ingredients, action.payload]
-        };
-      case ShoppingListActions.ADD_INGREDIENTS:
-        return {
-          ...state,
-          ingredients: [...state.ingredients, ...action.payload]
-        };
-      case ShoppingListActions.UPDATE_INGREDIENT:
-        const updatedIngredient = {
-          ...action.payload
-        };
-        const updatedIngredients = [...state.ingredients];
-        updatedIngredients[state.editedIngredientIndex] = updatedIngredient;
+export function ShoppingListReducer(
+  state = initialState,
+  action: ShoppingListActions.ShoppingListActionsType
+) {
+  switch (action.type) {
+    case ShoppingListActions.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action.payload]
+      };
+    case ShoppingListActions.ADD_INGREDIENTS:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, ...action.payload]
+      };
+    case ShoppingListActions.UPDATE_INGREDIENT:
+      const updatedIngredient = {
+        ...action.payload
+      };
+      const updatedIngredients = [...state.ingredients];
+      updatedIngredients[state.editedIngredientIndex] = updatedIngredient;
 
-        return {
-          ...state,
-          ingredients: updatedIngredients,
-          editedIngredient: null,
-          editedIngredientIndex: -1
-        };
-      case ShoppingListActions.DELETE_INGREDIENT:
-        return {
-          ...state,
-          ingredients: state.ingredients.filter(
-            (ing, i) => i !== state.editedIngredientIndex
-          ),
-          editedIngredient: null,
-          editedIngredientIndex: -1
-        };
-      case ShoppingListActions.START_EDIT:
-        return {
-          ...state,
-          editedIngredientIndex: action.payload,
-          editedIngredient: { ...state.ingredients[action.payload] }
-        };
-      case ShoppingListActions.STOP_EDIT:
-        return {
-          ...state,
-          editedIngredientIndex: -1,
-          editedIngredient: null
-        };
-      default:
-        return state;
-    }
+      return {
+        ...state,
+        ingredients: updatedIngredients,
+        editedIngredient: null,
+        editedIngredientIndex: -1
+      };
+    case ShoppingListActions.DELETE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: state.ingredients.filter(
+          (ing, i) => i !== state.editedIngredientIndex
+        ),
+        editedIngredient: null,
+        editedIngredientIndex: -1
+      };
+    case ShoppingListActions.START_EDIT:
+      return {
+        ...state,
+        editedIngredientIndex: action.payload,
+        editedIngredient: { ...state.ingredients[action.payload] }
+      };
+    case ShoppingListActions.STOP_EDIT:
+      return {
+        ...state,
+        editedIngredientIndex: -1,
+        editedIngredient: null
+      };
+    default:
+      return state;
+  }
 }
