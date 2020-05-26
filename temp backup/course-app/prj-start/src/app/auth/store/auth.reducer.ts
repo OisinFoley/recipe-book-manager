02ebe4c9996ff3e28485b902d4090df21e4bb172
OisinFoley <oisinfoley@yahoo.co.uk>
2020-05-26 11:59:46 -0700

    Pushing note files that have been created during the development of previous commits' work, but were unknowingly excluded from being staged

---
+++ temp backup/course-app/prj-start/src/app/auth/store/auth.reducer.ts
@@ -0,0 +1,46 @@
+import { User } from '../user/user.model';
+import * as AuthActions from './auth.actions';
+
+export interface State {
+  user: User;
+  authError: string;
+  loading: boolean;
+}
+
+const initialState = {
+  user: null,
+  authError: '',
+  loading: false
+};
+
+export function authReducer (
+  state = initialState,
+  action: AuthActions.AuthActionsType
+) {
+  switch (action.type) {
+    case AuthActions.AUTHENTICATE_SUCCESS:
+      const user = new User(
+        action.payload.email,
+        action.payload.userId,
+        action.payload.token,
+        action.payload.tokenExpirationDate
+      );
+      return {
+        ...state,
+        authError: null,
+        loading: false,
+        user
+      };
+    case AuthActions.AUTHENTICATE_FAIL:
+      return { ...state, authError: action.payload, loading: false };
+    case AuthActions.LOGIN_START:
+    case AuthActions.SIGNUP_START:
+      return { ...state, authError: null, loading: true };
+    case AuthActions.LOGOUT:
+      return { ...state, user: null };
+    case AuthActions.CLEAR_ERROR:
+      return { ...state, authError: null };
+    default:
+      return state;
+  }
+}
