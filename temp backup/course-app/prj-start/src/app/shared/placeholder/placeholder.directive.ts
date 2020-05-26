ef2ce7ecb1cb02c44a93ef31999ae041cb7f3d5a
OisinFoley <oisinfoley@yahoo.co.uk>
2020-05-26 15:35:46 -0700

    remove backup folder

--- temp backup/course-app/prj-start/src/app/shared/placeholder/placeholder.directive.ts
+++
@@ -1,9 +0,0 @@
-import { Directive, ViewContainerRef } from '@angular/core';
-
-@Directive({
-  selector: '[appPlaceholder]'
-})
-export class PlaceholderDirective {
-  // holds info about the view/component that we are about to embed a component in
-  constructor(public viewContainerRef: ViewContainerRef) { }
-}
